6c55ba062d3aecd5b6f899acf7c2276c455794b7
radwaadel17 <radwaadel959@gmail.com>
2026-07-07 00:11:14 +0300

    feat: refactor adaptive layout implementation and update imports

--- lib/features/dash-board/presentation/views/widgets/responsive-dashboard-view-body.dart
+++ lib/features/dash-board/presentation/views/widgets/responsive-dashboard-view-body.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:responsiveapp/features/dash-board/presentation/views/widgets/adaptive-layout.dart';
+import 'package:responsiveapp/core/adaptive-layout.dart';
 
 class ResponsiveDashBoardViewBody extends StatelessWidget {
   const ResponsiveDashBoardViewBody({super.key});
@@ -10,6 +10,10 @@ class ResponsiveDashBoardViewBody extends StatelessWidget {
       desktopLayout: (context) => SizedBox(),
       mobileLayout: (context) => SizedBox(),
       tabletLayout: (context) => SizedBox(),
-    );
+
+
+    ); 
   }
 }
+
+
